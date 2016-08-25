1711b9381f445a53b16c0379db8825ace1d8be8c
Conny <kawohl.bentodesign@gmail.com>
2016-08-25 17:52:22 +0200

    Adds readme

---
+++ reacti.sh
@@ -0,0 +1,8 @@
+echo "=== === ==="
+echo "Reactish 0.1"
+echo "=== === ==="
+echo "Please enter the dependencies you'd like installed"
+read userinput
+echo "Now installing: $userinput"
+npm install --save-dev $userinput
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Reactish
+## A small shell script that helps you quickly install react with the dependencies of your choice.
+
+===
+Work in Progress
